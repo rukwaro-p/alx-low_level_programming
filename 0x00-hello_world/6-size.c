278b92c32ab03f257813f190d3b8dbedc288abd5
rukwaro-p <rukwaropaul12@gmail.com>
2022-04-07 23:08:22 +0300

    Working with c and its printing

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("Size of a char: %lu byte(s)\n", (unasigned long)sizeof(d);
+printf("Size of an int: %lu byte(s)\n", (unasigned long)sizeof(a);
+printf("Size of a long int: %lu byte(s)\n", (unasigned long)sizeof(b);
+printf("Size of a long long int: %lu byte(s)\n", (unasigned long)sizeof(c);
+printf("Size of a float: %lu byte(s)\n", (unasigned long)sizeof(f);
+return (0);
+}

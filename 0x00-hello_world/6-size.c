9873557fb7243c43862ffb82aab1d89805af76f8
rukwaro-p <rukwaropaul12@gmail.com>
2022-04-08 00:12:46 +0300

    c is still fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ long long int c;
 char d;
 float f;
 
-printf("Size of a char: %lu byte(s)\n", (unasigned long)sizeof(d);
-printf("Size of an int: %lu byte(s)\n", (unasigned long)sizeof(a);
-printf("Size of a long int: %lu byte(s)\n", (unasigned long)sizeof(b);
-printf("Size of a long long int: %lu byte(s)\n", (unasigned long)sizeof(c);
-printf("Size of a float: %lu byte(s)\n", (unasigned long)sizeof(f);
+printf("Size of a char: %lu byte(s)\n", (unasigned long)sizeof(d));
+printf("Size of an int: %lu byte(s)\n", (unasigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unasigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unasigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unasigned long)sizeof(f));
 return (0);
 }

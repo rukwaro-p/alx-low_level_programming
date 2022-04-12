db54620322eed620fc923dc36220a6986a00c4de
Paul Rukwaro <rukwaropaul12@gmail.com>
2022-04-13 00:53:56 +0300

    task 9,10, 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two nums
+ * @num1: first num
+ * @num2: second num
+ * Return: The result of sum
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}

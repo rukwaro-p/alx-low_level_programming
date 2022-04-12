4bc18ed62525c53deb1b7bdd307fa62283d679a0
Paul Rukwaro <rukwaropaul12@gmail.com>
2022-04-13 00:28:32 +0300

    task 6,7, 8

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Finds absolute value of an integer
+ *@n: the character to be checked
+ * Return: the abs value of the given int
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}

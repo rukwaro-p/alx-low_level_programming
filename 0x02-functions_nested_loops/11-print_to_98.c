2c9f68828187af0101d86ba0fbaadc21a63897a3
Paul Rukwaro <rukwaropaul12@gmail.com>
2022-04-13 10:32:36 +0300

    task 11 take 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,16 @@ void print_to_98(int n)
 if (n >= 98)
 {
 while (n > 98)
-_putchar("%d, "+ n--);
-_putchar("%d\n"+ n);
+_putchar(n--);
+_putchar(",");
+_putchar("\n");
+
 }
 else
 {
 while (n < 98)
-_putchar("%d, "+ n++);
-_putchar("%d\n"+ n);
+_putchar(n++);
+_putchar(",");
+_putchar("\n");
 }
 }
